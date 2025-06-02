6d28fe95f3d399e1627d939af36f4e6d1238c938
cyril-k-u <cyril.k@evoqins.com>
2025-06-02 11:17:01 +0530

    'test2'

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app1 = FastAPI(title="My FastAPI App!!!")
 
-@app.get('/')
+
+
+
+@app1.get('/hello')
 def say_hi():
-  return 'Hiii'
+  return 'Hello from the edited copy'
